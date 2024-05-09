d6b96ca6e8ad9ea1b6e08760bd8c0cb0a46ef212
q210208 <f.mahy@student.helmo.be>
2024-05-09 09:33:34 +0200

    first commit

---
+++ src/components/Common/PageWrapper.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactElement } from 'react';
+import Header from '../Header/Header.tsx';
+type PageWrapperProps = {
+    children?: ReactElement;
+};
+
+const PageWrapper: FC<PageWrapperProps> = ({ children }) => {
+    return (
+        <div>
+            <Header />
+            {children}
+    {/* Footer */}
+    </div>
+);
+};
+
+export default PageWrapper;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+
+    return (
+        <header className="background-primary-color border-b h-[12.5vh]">
+            COUCOU
+        </header>
+);
+}
+
+export default Header;
